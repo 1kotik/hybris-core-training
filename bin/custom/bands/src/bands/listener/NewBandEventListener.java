9b7e9cb6a3319198d330663608f834001f762052
kotik <133994705+1kotik@users.noreply.github.com>
2026-01-19 18:07:44 +0300

    News are now catalog-aware. Stages items are filtered out via restriction (#5)

--- bin/custom/bands/src/bands/listener/NewBandEventListener.java
+++ bin/custom/bands/src/bands/listener/NewBandEventListener.java
@@ -2,7 +2,6 @@ package bands.listener;
 
 import bands.model.BandModel;
 import bands.model.NewsModel;
-import bands.service.NewsService;
 import de.hybris.platform.servicelayer.event.events.AfterItemCreationEvent;
 import de.hybris.platform.servicelayer.event.impl.AbstractEventListener;
 import de.hybris.platform.servicelayer.model.ModelService;
@@ -10,17 +9,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
-import java.util.NoSuchElementException;
 
 @Component
 public class NewBandEventListener extends AbstractEventListener<AfterItemCreationEvent> {
     private final ModelService modelService;
-    private final NewsService newsService;
 
     @Autowired
-    public NewBandEventListener(ModelService modelService, NewsService newsService) {
+    public NewBandEventListener(ModelService modelService) {
         this.modelService = modelService;
-        this.newsService = newsService;
     }
 
     @Override
@@ -32,23 +28,10 @@ public class NewBandEventListener extends AbstractEventListener<AfterItemCreatio
         if (source instanceof BandModel band) {
             final NewsModel news = modelService.create(NewsModel.class);
             final String bandName = band.getName();
-            news.setCode(generateUniqueCode());
             news.setHeadline(String.format("New band: %s", bandName));
             news.setContent(String.format("New band in town: %s", bandName));
             news.setDate(new Date());
             modelService.save(news);
-        }
-    }
-
-    private String generateUniqueCode() {
-        final String prefix = "N";
-        try {
-            final NewsModel newsModel = newsService.findLatest();
-            final String code = newsModel.getCode();
-            int intCodeValue = Integer.parseInt(code.substring(1));
-            return prefix + ++intCodeValue;
-        } catch (final NoSuchElementException e) {
-            return prefix + "1";
         }
     }
 
